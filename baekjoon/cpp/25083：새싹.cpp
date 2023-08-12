6931d5f80d69398be570598849ef943ce77cc1ef
seung-doitnow <seung.doitnow@gmail.com>
2023-08-12 23:44:21 +0900

    25083 : 새싹

---
+++ baekjoon/cpp/25083：새싹.cpp
@@ -0,0 +1,16 @@
+// 25083번 : 새싹
+// 주소 : https://www.acmicpc.net/problem/25083
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+}
