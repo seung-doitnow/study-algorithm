78c0943b600ab2cf22d7be9f29e2af954a37430a
이승일 <141153010+seung-doitnow@users.noreply.github.com>
2023-08-19 22:18:04 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,8 +1,3 @@
 function solution(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    
-    return sum / arr.length;
+    return arr.reduce((sum, num) => sum + num) / arr.length;
 }
