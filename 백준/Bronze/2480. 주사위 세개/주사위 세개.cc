0da0504d77234e83aa472a1baf53ea7022acbdf7
이승일 <141153010+seung-doitnow@users.noreply.github.com>
2023-08-25 20:18:02 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int dice1, dice2, dice3;
+	cin >> dice1 >> dice2 >> dice3;
+
+	if (dice1 == dice2 && dice2 == dice3)
+		cout << 10'000 + (dice1 * 1'000);
+	else if (dice1 == dice2 || dice1 == dice3)
+		cout << 1'000 + (dice1 * 100);
+	else if (dice2 == dice3)
+		cout << 1'000 + (dice2 * 100);
+	else
+	{
+		int maxDiceValue = dice1;
+		if (maxDiceValue < dice2)
+			maxDiceValue = dice2;
+		
+		if (maxDiceValue < dice3)
+			maxDiceValue = dice3;
+
+		cout << maxDiceValue * 100;
+	}
+}
