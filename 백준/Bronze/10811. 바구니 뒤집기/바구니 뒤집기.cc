c8eced7010f9847522acfc2f6749c8eef937d1a0
이승일 <141153010+seung-doitnow@users.noreply.github.com>
2023-08-27 12:57:16 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+
+	int baskets[101] = { 0 };
+	for (int i = 1; i <= N; i++)
+		baskets[i] = i;
+
+	for (int count = 0; count < M; count++)
+	{
+		int i, j;
+		cin >> i >> j;
+
+		while (i <= j)
+		{
+			int number = baskets[i];
+			baskets[i] = baskets[j];
+			baskets[j] = number;
+
+			i++;
+			j--;
+		}
+	}
+
+	for (int i = 1; i <= N; i++)
+		cout << baskets[i] << ' ';
+}
