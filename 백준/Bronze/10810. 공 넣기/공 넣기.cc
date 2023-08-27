c440a164b5e9d0cad524d6f84baaeed15df6c697
이승일 <141153010+seung-doitnow@users.noreply.github.com>
2023-08-27 12:56:50 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+
+	int baskets[101] = { 0 };
+	for (int count = 0; count < M; count++)
+	{
+		int i, j, k;
+		cin >> i >> j >> k;
+
+		while (i <= j)
+		{
+			baskets[i] = k;
+			i++;
+		}
+	}
+
+	for (int i = 1; i <= N; i++)
+		cout << baskets[i] << ' ';
+}
