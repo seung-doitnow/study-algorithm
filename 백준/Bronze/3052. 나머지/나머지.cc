c27fb894a7f50658bd977884d9f06ac30d6e5420
이승일 <141153010+seung-doitnow@users.noreply.github.com>
2023-08-27 12:57:10 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int remainders[42] = { 0 };
+	for (int i = 0; i < 10; i++)
+	{
+		int number;
+		cin >> number;
+
+		remainders[number % 42]++;
+	}
+
+	int count = 0;
+	for (int i = 0; i < 42; i++)
+	{
+		if (remainders[i] != 0)
+			count++;
+	}
+
+	cout << count;
+}
