d46685888297ccd8b36a77e782bdc2fa58f1adb6
이승일 <141153010+seung-doitnow@users.noreply.github.com>
2023-08-27 12:56:37 +0900

    [Bronze III] Title: 최소, 최대, Time: 104 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	int min = 1'000'001;
+	int max = -1'000'001;
+	for (int i = 0; i < N; i++)
+	{
+		int integer;
+		cin >> integer;
+
+		if (integer < min)
+			min = integer;
+
+		if (max < integer)
+			max = integer;
+	}
+
+	cout << min << ' ' << max;
+}
