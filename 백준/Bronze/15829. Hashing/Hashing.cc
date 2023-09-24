6a6f4283a6126a88063b35c06b72ed4692fa7990
이승일 <141153010+seung-doitnow@users.noreply.github.com>
2023-09-24 16:37:16 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -8,16 +8,16 @@ int main()
 	char string[51];
 	cin >> length >> string;
 
-	int hashValue = (int)(string[0]) - 'a' + 1;
-	for (int i = 1; i < length; i++)
+	long long hashValue = 0;
+	long long multipleValue = 1;
+	for (int i = 0; i < length; i++)
 	{
 		int digit = (int)(string[i]) - 'a' + 1;
-		int power = 31;
-		for (int j = 1; j < i; j++)
-			power = power * 31 % 1234567891;
 
-		hashValue += digit * power % 1234567891;
+		hashValue = hashValue + ((digit * multipleValue) % 1234567891);
+
+		multipleValue = (multipleValue * 31) % 1234567891;
 	}
 
-	cout << hashValue;
+	cout << hashValue % 1234567891;
 }
